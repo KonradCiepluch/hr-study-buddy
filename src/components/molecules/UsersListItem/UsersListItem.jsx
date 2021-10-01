acddda187f39db736ba955c80d3f8a9d3e9b548f
Konrad Ciepluch <konrad190008@wp.pl>
2021-10-01 16:53:22 +0200

    UsersProvider added

--- src/components/molecules/UsersListItem/UsersListItem.jsx
+++ src/components/molecules/UsersListItem/UsersListItem.jsx
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Wrapper from './UserListItem.styles';
 import Grade from 'components/atoms/Grade/Grade.styles';
 import DeleteButton from 'components/atoms/DeleteButton/DeleteButton';
+import { UsersContext } from 'providers/UsersProvider';
+import UserShape from 'components/types';
 
-const UsersListItem = ({ deleteUser, user: { name, average, attendance = '0%' } }) => (
-  <Wrapper key={name}>
-    <Grade grade={average}>{average}</Grade>
-    <div>
-      <p>{name}</p>
-      <p>attendance {attendance}</p>
-    </div>
-    <DeleteButton onClick={() => deleteUser(name)} />
-  </Wrapper>
-);
+const UsersListItem = ({ user: { name, average, attendance = '0%' } }) => {
+  const { deleteUser } = useContext(UsersContext);
+
+  return (
+    <Wrapper key={name}>
+      <Grade grade={average}>{average}</Grade>
+      <div>
+        <p>{name}</p>
+        <p>attendance {attendance}</p>
+      </div>
+      <DeleteButton onClick={() => deleteUser(name)} />
+    </Wrapper>
+  );
+};
 
 UsersListItem.propTypes = {
-  user: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    average: PropTypes.string.isRequired,
-    attendance: PropTypes.string,
-  }),
-  deleteUser: PropTypes.func.isRequired,
+  user: PropTypes.shape(UserShape),
 };
 
 export default UsersListItem;
--- src/views/Root.jsx
+++ src/views/Root.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Wrapper } from './Root.styles';
 import AddUser from './AddUser';
 import Dashboard from './Dashboard';
@@ -6,71 +6,27 @@ import MainTemplate from 'components/templates/MainTemplate/MainTemplate';
 import { ThemeProvider } from 'styled-components';
 import theme from 'assets/styles/theme';
 import GlobalStyle from 'assets/styles/globalStyle';
-import usersData from 'data/users';
+import UsersProvider from 'providers/UsersProvider';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const mockApi = () =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (usersData) {
-        resolve([...usersData]);
-      } else {
-        reject({ message: 'Error' });
-      }
-    }, 2000);
-  });
-
-const initialFormState = { name: '', attendance: '', average: '' };
-
 const Root = () => {
-  const [formValues, setFormValues] = useState(initialFormState);
-
-  const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleInputChange = (e) => {
-    setFormValues({ ...formValues, [e.target.name]: e.target.value });
-  };
-
-  useEffect(() => {
-    setIsLoading(true);
-    mockApi()
-      .then((data) => {
-        setIsLoading(false);
-        setUsers(data);
-      })
-      .catch((error) => console.log(error));
-  }, []);
-
-  const deleteUser = (name) => {
-    const filteredUsers = users.filter((user) => user.name !== name);
-    setUsers(filteredUsers);
-  };
-
-  const handleAddUser = (e) => {
-    e.preventDefault();
-    const newUser = { ...formValues };
-    if (newUser.name && newUser.attendance && newUser.average) {
-      setUsers([newUser, ...users]);
-      setFormValues(initialFormState);
-    }
-  };
-
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <MainTemplate>
-          <Wrapper>
-            <Switch>
-              <Route path="/add-user">
-                <AddUser handleInputChange={handleInputChange} handleAddUser={handleAddUser} formValues={formValues} />
-              </Route>
-              <Route path="/">
-                <Dashboard users={users} isLoading={isLoading} deleteUser={deleteUser} />
-              </Route>
-            </Switch>
-          </Wrapper>
+          <UsersProvider>
+            <Wrapper>
+              <Switch>
+                <Route path="/add-user">
+                  <AddUser />
+                </Route>
+                <Route path="/">
+                  <Dashboard />
+                </Route>
+              </Switch>
+            </Wrapper>
+          </UsersProvider>
         </MainTemplate>
       </ThemeProvider>
     </Router>
