3c2e10ce49ffa001557f014ec675f484f6b62be6
Konrad Ciepluch <konrad190008@wp.pl>
2021-10-13 00:06:33 +0200

    Added tests and NewsSection

--- src/components/atoms/Title/Title.styles.js
+++ src/components/atoms/Title/Title.styles.js
@@ -2,5 +2,5 @@ import styled from 'styled-components';
 
 export const Title = styled.h1`
   font-size: ${({ theme }) => theme.fontSize.xl};
-  color: ${({ theme }) => theme.colors.lightPurple};
+  color: ${({ theme }) => theme.colors.darkGrey};
 `;
