b9300451c3023fb088fd6307bf2fb1a68adf9b1c
Konrad Ciepluch <konrad190008@wp.pl>
2021-10-22 11:44:03 +0200

    Added Root tests and updated SearchBar tests

--- src/components/organisms/SearchBar/SearchBar.test.js
+++ src/components/organisms/SearchBar/SearchBar.test.js
@@ -1,16 +1,8 @@
 import { render, screen, fireEvent, waitFor } from 'test-utils';
 import '@testing-library/jest-dom';
-import { setupServer } from 'msw/node';
-import { handlers } from 'mocks/handlers';
 import SearchBar from './SearchBar';
 
-const server = setupServer(...handlers);
-
 describe('Search Bar', () => {
-  beforeAll(() => server.listen());
-  afterEach(() => server.resetHandlers());
-  afterAll(() => server.close());
-
   it('Renders the component', () => {
     render(<SearchBar />);
     screen.getByText('Teacher');
@@ -20,16 +12,16 @@ describe('Search Bar', () => {
   it('Displays users when search phrase is matching', async () => {
     render(<SearchBar />);
     const input = screen.getByPlaceholderText('Search');
-    fireEvent.change(input, { target: { value: 'ad' } });
+    fireEvent.change(input, { target: { value: 'Agn' } });
 
-    await screen.findByText(/Adam Romański/);
+    await screen.findByText(/Agnes/);
   });
 
   it('Checks if list is present when search phrase is empty', async () => {
     render(<SearchBar />);
     const input = screen.getByPlaceholderText('Search');
-    fireEvent.change(input, { target: { value: 'ad' } });
-    await screen.findByText(/Adam Romański/);
+    fireEvent.change(input, { target: { value: 'Agn' } });
+    await screen.findByText(/Agnes/);
     fireEvent.change(input, { target: { value: '' } });
 
     await waitFor(() => {
